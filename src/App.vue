aa250e96294be9dfabd9f98ccc26d0cc1fcdcbc0
yuki96_96 <yuki1015wo@gmail.com>
2022-04-18 15:58:46 +0900

    loginPage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">ログイン</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            color="blue"
+            prepend-icon="mdi-account-circle"
+            label="ユーザー名"
+            v-model="name"
+          ></v-text-field>
+          <v-text-field
+            color="blue"
+            v-bind:type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+            prepend-icon="mdi-lock"
+            v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            label="パスワード"
+            v-model="password"
+          ></v-text-field>
+          <v-card-actions>
+            <v-btn color="info" @click="submit">ログイン</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
   </v-app>
 </template>
 
@@ -13,9 +37,17 @@ export default defineComponent({
   name: "App",
 
   data() {
+    //showPassword: false;
     return {
-      //
+      showPassword: false,
+      name: "",
+      password: "",
     };
+  },
+  methods: {
+    submit() {
+      console.log(this.name, this.password);
+    },
   },
 });
 </script>
